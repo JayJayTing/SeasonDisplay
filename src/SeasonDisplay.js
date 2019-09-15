f861221bee33780587cdff1c73c66c4f65d96074
JayJayTing <tingjayj@gmail.com>
2019-09-15 19:12:05 -0400

    completed this practice

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,9 @@
 import React from "react";
+import "./SeasonDisplay.css";
+const seasonConfig = {
+	summer: { text: "lets hit the beach", icon: "sun" },
+	winter: { text: "Burr it is cold!", icon: "snowflake" }
+};
 
 const getSeason = (lat, month) => {
 	if (month > 2 && month < 9) {
@@ -9,9 +14,12 @@ const getSeason = (lat, month) => {
 };
 export default function SeasonDisplay(props) {
 	let season = getSeason(props.lattitude, new Date().getMonth());
+	const { text, icon } = seasonConfig[season];
 	return (
-		<div>
-			{season === "winter" ? "burr it's chilly!" : "lets hit the beach!"}
+		<div className={`season-display ${season}`}>
+			<i className={`icon-left massive ${icon} icon`} />
+			<h1>{text}</h1>
+			<i className={`icon-right massive ${icon} icon`} />
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import { classImplements } from "@babel/types";
 
 function App() {
 	const [state, setState] = useState({ lat: null });
+	function renderContent() {
+		if (state.errorMesage && !state.lat) {
+			return <div> Error: {state.errorMesage}</div>;
+		}
+		if (!state.errorMesage && state.lat) {
+			return <SeasonDisplay lat={state.lat} />;
+		}
 
+		return <Spinner message="Please accept location request" />;
+	}
 	useEffect(() => {
 		window.navigator.geolocation.getCurrentPosition(
 			position => {
@@ -16,11 +27,7 @@ function App() {
 		);
 	}, []);
 
-	return (
-		<div>
-			<SeasonDisplay latitude={state.lat} error={state.errorMessage} />
-		</div>
-	);
+	return <div className="border red">{renderContent()}</div>;
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
